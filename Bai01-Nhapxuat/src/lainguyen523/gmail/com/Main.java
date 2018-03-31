ade0bcc47419f6fc51faa3b9ac5ada72b2fe76e4
HOCVIEN <HOCVIEN@DESKTOP-LMHOMK8>
2018-03-31 16:32:21 +0700

    Oke

---
+++ Bai01-Nhapxuat/src/lainguyen523/gmail/com/Main.java
@@ -0,0 +1,18 @@
+package lainguyen523.gmail.com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 1;
+		float b = 3.14;
+		System.out.print(a);
+		System.out.print(b);
+		System.out.print("=====");
+		System.out.println(a);
+		System.out.println(b);
+		
+				
+
+	}
+
+}
