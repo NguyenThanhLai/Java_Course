9b565381e8840ef440e8f1b252601b4b3fcbfe29
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-04-21 15:26:46 +0700

    Update Daotu.java

--- Bai01_th01/src/lainguyen523/gmail/com/Daotu.java
+++ Bai01_th01/src/lainguyen523/gmail/com/Daotu.java
@@ -1,10 +1,19 @@
 package lainguyen523.gmail.com;
 
-public class Daotu {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class DaoTu {
+	public static String thuchien(String str) {
+		int r = 0;
+		String a = " ";
+		for (int i = 0; str.length() > i; i++) 
+		{
+			if (str.substring(i, i + 1).compareTo(" ") == 0) 
+			{
+				a = str.substring(r, i) + " " + a;
+				r = i;
+			}
+		}
+		a = str.substring(r, str.length()) + " " + a;
+		a = a.substring(1);
+		return a.trim();
 	}
-
 }
