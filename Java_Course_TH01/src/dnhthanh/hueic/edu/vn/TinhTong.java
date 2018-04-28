97e72bccd368726c7065c132172ffea9c4d4610f
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-04-28 14:12:40 +0700

    Update TinhTong.java

--- Java_Course_TH01/src/dnhthanh/hueic/edu/vn/TinhTong.java
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/TinhTong.java
@@ -6,7 +6,10 @@ public class TinhTong {
 		float S = 0;
 		int size = a.length;//kích thước của mảng
 		//Viết hàm thực hiện việc cộng các phần tử của mảng a
-		
+		for (int i = 0; i < size; i ++){
+			S += a[i];
+		}
 		return S;
 	}
+
 }
