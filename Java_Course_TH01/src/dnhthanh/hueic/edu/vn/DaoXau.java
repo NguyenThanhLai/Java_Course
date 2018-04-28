9f21df1be1d7c49788b545d1ce5213705b2779cc
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-04-28 14:11:04 +0700

    Update DaoXau.java

--- Java_Course_TH01/src/dnhthanh/hueic/edu/vn/DaoXau.java
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/DaoXau.java
@@ -4,7 +4,10 @@ public class DaoXau {
 	public static String thuchien(String s) {
 		String ns = new String();
 		//Viết nội dung xử lý xâu s cho ra xâu ns, trong đó ns là xâu đảo xâu.
-		
+		char []s1 = s.toCharArray();
+		for (int i = s1.length - 1; i >= 0; i--){
+			ns += s1[i];
+		}
 		return ns;
 	}
 }
