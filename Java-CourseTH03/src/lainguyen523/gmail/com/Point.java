06235a8cd5515b03ec50918a0a09ee75aacd7bed
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-04-28 16:37:30 +0700

    Add files via upload

---
+++ Java-CourseTH03/src/lainguyen523/gmail/com/Point.java
@@ -0,0 +1,13 @@
+package akimkute05.gmail.com;
+
+public class Point {
+	private float x;//Tọa độ x
+	private float y;//Tọa độ y
+	
+	public Point() {
+		this.x = Math.round(Math.random()*50);
+		this.y = Math.round(Math.random()*50);
+	}
+	
+	//Tạo getters và setters cho x và y
+}
