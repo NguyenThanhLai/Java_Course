2c3bf0e4fcac62d301ec14a5a994fe445f8de101
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-04-28 14:09:26 +0700

    Add files via upload

---
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/GPT.java
@@ -0,0 +1,9 @@
+package dnhthanh.hueic.edu.vn;
+
+public class GPT {
+
+	public static void thuchien(float a, float b, float c) {
+		//Viết nội dung giải phương trình bậc 2 vào đây
+		
+	}
+}
---
+++ Java_Course_TH01/src/dnhthanh/hueic/edu/vn/TinhTong.java
@@ -0,0 +1,12 @@
+package dnhthanh.hueic.edu.vn;
+
+public class TinhTong {
+
+	public static float thuchien(float[] a) {
+		float S = 0;
+		int size = a.length;//kích thước của mảng
+		//Viết hàm thực hiện việc cộng các phần tử của mảng a
+		
+		return S;
+	}
+}
