23204f1f938104e382f0474abcedaf9af7864e42
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-05-05 13:56:27 +0700

    Update Round.java

--- Java-CourseTH03/src/lainguyen523/gmail/com/Round.java
+++ Java-CourseTH03/src/lainguyen523/gmail/com/Round.java
@@ -10,10 +10,35 @@ public class Round {
 	}
 	
 	//Tạo getters và setters cho R và O
-	
+
+
+	public float getR() {
+		return R;
+	}
+
+	public void setR(float r) {
+		R = r;
+	}
+
+	public Point getO() {
+		return O;
+	}
+
+	public void setO(Point o) {
+		O = o;
+	}
+
 	//Xét vị trí tương đối điểm với đường tròn: nếu nằm trong - trả về -1; nếu nằm ngoài - trả về 1; nếu nằm trên - trả về 0
 	public int ViTriTuongDoiDiemVoiDuongTron(Point A) {
-		
-		return 0;
+		//bình phương khoảng cách đoạn OA
+		float OAbp = (float) Math.sqrt((Math.pow(O.getX() - A.getX(), 2)) + (Math.pow(O.getY() - A.getY(), 2)));
+		//xét vị trí tương đối
+		if (OAbp == R){
+			return 0;
+		}else if (OAbp < R){
+			return -1;
+		}else
+			return 1;
+
 	}
 }
