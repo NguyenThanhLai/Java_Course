65997fcfd171ddbf06e63986eb481146db3857dd
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-05-05 16:30:34 +0700

    Add files via upload

---
+++ Java-CourseTH05/src/lainguyen523/gmail/com/AVector.java
@@ -0,0 +1,11 @@
+package lainguyen523.gmail.com;
+
+public abstract class AVector {
+	public abstract double TinhDoDai(); 
+	
+	public abstract double SinGoc();
+	
+	public abstract double TrucGiao();
+	
+
+} 
---
+++ Java-CourseTH05/src/lainguyen523/gmail/com/Vector2D.java
@@ -0,0 +1,28 @@
+package lainguyen523.gmail.com;
+
+public class Vector2D extends AVector {
+	public Vector2D(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public double tinhDoDai() {
+		int a = 0;
+		// TODO Auto-generated method stub
+		return Math.abs(a);
+	}
+
+	@Override
+	public double sinGoc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double trucGiao() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
