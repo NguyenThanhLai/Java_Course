9b5c5c80e63ce6c3169a4fad18d1faeb950a81af
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-05-05 13:54:21 +0700

    Update MainClass.java

--- Java-CourseTH03/src/lainguyen523/gmail/com/MainClass.java
+++ Java-CourseTH03/src/lainguyen523/gmail/com/MainClass.java
@@ -17,7 +17,19 @@ public class MainClass {
 		for(int i=0; i<numR; i++)
 			rounds.add(new Round());
 		//Gọi phương thức xét vị trí tương đối
-		
+		for (int i = 0; i < numR; i++) {
+			for (int j = 0; j < numP; j++) {
+				int KQ;
+				KQ = rounds.get(i).ViTriTuongDoiDiemVoiDuongTron(points.get(j));
+				if (KQ == 0) {
+					System.out.println("Điểm " + (j + 1) + " nằm trên đường tròn " + (i + 1));
+				}else if (KQ == -1) {
+					System.out.println("Điểm " + (j + 1) + " nằm trong đường tròn " + (i + 1));
+				}else if(KQ == 1){
+					System.out.println("Điểm " + (j + 1) + " nằm ngoài đường tròn " + (i + 1));
+				}
+			}
+		}
 	}
 
 }
