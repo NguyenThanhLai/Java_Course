3feab500680277bf4d58c1ef247603a4958256b9
NguyenThanhLai <37949708+NguyenThanhLai@users.noreply.github.com>
2018-05-19 14:41:26 +0700

    Update Employers.java

--- BaiTap/src/lainguyen523/gmail/com/Employers.java
+++ BaiTap/src/lainguyen523/gmail/com/Employers.java
@@ -3,32 +3,58 @@ package lainguyen523.gmail.com;
 import java.util.ArrayList;
 
 public class Employers implements iHuman {
-	private ArrayList<Employer> Employers = new ArrayList<Employer>();
+
+	private ArrayList<Employer> employers = new ArrayList<Employer>();
+
+	public ArrayList<Employer> getEmployers() {
+		return employers;
+	}
+
+	public void setEmployers(ArrayList<Employer> employers) {
+		this.employers = employers;
+	}
 
 	@Override
 	public Object timKiem(int from, int to) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Employer> timtuoi = new ArrayList<Employer>();
+		for (int i = 0; i < employers.size(); i++) {
+			if (employers.get(i).gettinhTuoi() <= to && employers.get(i).gettinhTuoi() >= from) {
+				timtuoi.add(employers.get(i));
+			}
+		}
+		return timtuoi;
 	}
 
 	@Override
-	public Object timKiem(boolean gender) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object timKiem(boolean gioitinh) {
+		ArrayList<Employer> timgt = new ArrayList<Employer>();
+		for (int i = 0; i < employers.size(); i++) {
+			if (employers.get(i).getGioitinh() == gioitinh ) {
+				timgt.add(employers.get(i));
+			}
+		}
+		return timgt;
 	}
 
 	@Override
-	public Object timKiem(String ten) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object timKiem(String hoten) {
+		ArrayList<Employer> timten = new ArrayList<Employer>();
+		for (int i = 0; i < employers.size(); i++) {
+			if (employers.get(i).getHoten().indexOf(hoten)>=0) {
+				timten.add(employers.get(i));
+			}
+		}
+		return timten;
 	}
-
-	public ArrayList<Employer> getEmployers() {
-		return Employers;
-	}
-
-	public void setEmployer(ArrayList<Employer> Employers) {
-		this.Employers = Employers;
+	
+	public Object timKiem(Float tinhLuong) {
+		ArrayList<Employer> timluong = new ArrayList<Employer>();
+		for (int i = 0; i < employers.size(); i++) {
+			if (employers.get(i).tinhLuong() == tinhLuong ) {
+				timluong.add(employers.get(i));
+			}
+		}
+		return timluong;
 	}
 
 }
